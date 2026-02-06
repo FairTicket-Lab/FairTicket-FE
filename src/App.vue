6d3effd7e566ab9fbaca66121dd243616d685381
세빈 권 <a856412@ajou.ac.kr>
2026-02-06 22:31:21 +0900

    chore: 프론트엔드 개발 환경 초기 세팅

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="bg-blue-500 text-white p-8">
+    파란 배경 (Tailwind)
+  </div>
+  <div class="bg-red-500 text-white p-8 mt-4">
+    빨간 배경 (Tailwind)
+  </div>
+  <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+    버튼 테스트
+  </button>
+</template>
